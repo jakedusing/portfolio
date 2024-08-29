a74e53babdae17079a63deb2111b9ca990bfd892
Jake Dusing <dusingjake@gmail.com>
2024-08-29 16:27:45 -0500

    update button styles and add typing animation

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,26 +1,50 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            Hello, I`m Jake
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, I`m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Jake",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                "We produce food for Hamsters",
+                1000,
+                "We produce food for Guinea Pigs",
+                1000,
+                "We produce food for Chinchillas",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
-          <p className="text-[#ADB7BE] mb-6 text-lg lg:text-xl">
+          <p className="text-[#ADB7BE] mb-6 text-base sm:text-lg lg:text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
             cumque, reprehenderit esse voluptatum ipsa repellat minima quas
             architecto cupiditate dicta magni nisi? Dicta debitis at adipisci
             unde hic voluptate suscipit.
           </p>
           <div>
-            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
               Contact Me
             </button>
-            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
-              Download Resume
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download Resume
+              </span>
             </button>
           </div>
         </div>
