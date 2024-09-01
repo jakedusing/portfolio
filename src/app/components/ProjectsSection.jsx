ff177588ad4c37e1f1edb8fe96fc35fe1594af2e
Jake Dusing <dusingjake@gmail.com>
2024-09-01 13:20:19 -0500

    update projects section

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
 
 const projectsData = [
   {
@@ -8,6 +10,8 @@ const projectsData = [
     description: "A campground review website",
     image: "/images/projects/yelpcamp.JPG",
     tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 1,
@@ -15,6 +19,8 @@ const projectsData = [
     description: "Online multiplayer shooter game",
     image: "/images/projects/blaster.JPG",
     tag: ["All", "Games"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 1,
@@ -22,22 +28,53 @@ const projectsData = [
     description: "Kitchen assembly game",
     image: "/images/projects/kitchenchaos.JPG",
     tag: ["All", "Games"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
 ];
 
 const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
   return (
     <>
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-6">
         My Projects
       </h2>
-      <div>
-        {projectsData.map((project) => (
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Games"
+          isSelected={tag === "Games"}
+        />
+      </div>
+      <div className="grid lg:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project) => (
           <ProjectCard
             key={project.id}
             title={project.title}
             description={project.description}
             imgUrl={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
           />
         ))}
       </div>
