d6bce039f4029f630d0cef4231483dd14aa595d5
Jake Dusing <dusingjake@gmail.com>
2025-04-19 20:44:48 -0500

    update AboutSection education

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -26,6 +26,8 @@ const TAB_DATA = [
     content: (
       <ul className="list-disc pl-2">
         <li>Bachelors in Computer Science - Governors State University</li>
+        <li>Unreal Engine 5 C++ Multiplayer Shooter - Udemy</li>
+        <li>Unreal Engine 5 Blueprints: First Person Shooter - Udemy</li>
         <li>The Modern Javascript Bootcamp - Udemy</li>
         <li>The Web Developer Bootcamp - Udemy</li>
       </ul>
